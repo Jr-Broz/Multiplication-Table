68f725fb4b8876a955067158e5b9b5f2029a2cbf
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-06-23 14:59:15 -0300

    Created script.js

---
+++ script.js
@@ -0,0 +1,112 @@
+for(let i = 1;i <= 10; i++){
+  for(let j = 1;j <= 10; j++){
+
+    console.log(i + "x" + j + "=" + (i * j));
+
+    
+  }
+}
+
+
+/*Output:
+script.js:4 1x1=1
+script.js:4 1x2=2
+script.js:4 1x3=3
+script.js:4 1x4=4
+script.js:4 1x5=5
+script.js:4 1x6=6
+script.js:4 1x7=7
+script.js:4 1x8=8
+script.js:4 1x9=9
+script.js:4 1x10=10
+script.js:4 2x1=2
+script.js:4 2x2=4
+script.js:4 2x3=6
+script.js:4 2x4=8
+script.js:4 2x5=10
+script.js:4 2x6=12
+script.js:4 2x7=14
+script.js:4 2x8=16
+script.js:4 2x9=18
+script.js:4 2x10=20
+script.js:4 3x1=3
+script.js:4 3x2=6
+script.js:4 3x3=9
+script.js:4 3x4=12
+script.js:4 3x5=15
+script.js:4 3x6=18
+script.js:4 3x7=21
+script.js:4 3x8=24
+script.js:4 3x9=27
+script.js:4 3x10=30
+script.js:4 4x1=4
+script.js:4 4x2=8
+script.js:4 4x3=12
+script.js:4 4x4=16
+script.js:4 4x5=20
+script.js:4 4x6=24
+script.js:4 4x7=28
+script.js:4 4x8=32
+script.js:4 4x9=36
+script.js:4 4x10=40
+script.js:4 5x1=5
+script.js:4 5x2=10
+script.js:4 5x3=15
+script.js:4 5x4=20
+script.js:4 5x5=25
+script.js:4 5x6=30
+script.js:4 5x7=35
+script.js:4 5x8=40
+script.js:4 5x9=45
+script.js:4 5x10=50
+script.js:4 6x1=6
+script.js:4 6x2=12
+script.js:4 6x3=18
+script.js:4 6x4=24
+script.js:4 6x5=30
+script.js:4 6x6=36
+script.js:4 6x7=42
+script.js:4 6x8=48
+script.js:4 6x9=54
+script.js:4 6x10=60
+script.js:4 7x1=7
+script.js:4 7x2=14
+script.js:4 7x3=21
+script.js:4 7x4=28
+script.js:4 7x5=35
+script.js:4 7x6=42
+script.js:4 7x7=49
+script.js:4 7x8=56
+script.js:4 7x9=63
+script.js:4 7x10=70
+script.js:4 8x1=8
+script.js:4 8x2=16
+script.js:4 8x3=24
+script.js:4 8x4=32
+script.js:4 8x5=40
+script.js:4 8x6=48
+script.js:4 8x7=56
+script.js:4 8x8=64
+script.js:4 8x9=72
+script.js:4 8x10=80
+script.js:4 9x1=9
+script.js:4 9x2=18
+script.js:4 9x3=27
+script.js:4 9x4=36
+script.js:4 9x5=45
+script.js:4 9x6=54
+script.js:4 9x7=63
+script.js:4 9x8=72
+script.js:4 9x9=81
+script.js:4 9x10=90
+script.js:4 10x1=10
+script.js:4 10x2=20
+script.js:4 10x3=30
+script.js:4 10x4=40
+script.js:4 10x5=50
+script.js:4 10x6=60
+script.js:4 10x7=70
+script.js:4 10x8=80
+script.js:4 10x9=90
+script.js:4 10x10=100
+*/
